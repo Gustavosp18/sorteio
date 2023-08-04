8e990d36d6404579de0b32b16b4103fd080873fb
Gustavosp18 <133408925+Gustavosp18@users.noreply.github.com>
2023-08-04 16:39:53 -0300

    Initial commit
    .
    
    .

---
+++ aula.js
@@ -0,0 +1,13 @@
+console.log(math.random());
+console.log(mth.random() * 20)
+console.log(math.random(7 - 3 + 1) + 3);
+
+console.log(math.round(3.1));
+console.log(math.round(3.9));
+
+
+console.log(math.floor(3.1));
+console.log(math.floor(3.9));
+
+console.log(math.ceil(3.1));
+console.log(math.ceil(3.9));
